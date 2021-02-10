4f525397ca0a1005d2fbe352c0d5f14386b07c6b
llewyn <kbj2060@naver.com>
2021-02-10 15:17:40 +0900

    [Bug Fix] Multi Dex Problem Fixed

--- app/src/main/java/com/plantpoint/MyApplication.kt
+++ app/src/main/java/com/plantpoint/MyApplication.kt
@@ -2,6 +2,10 @@ package com.plantpoint
 
 import android.app.Application
 import android.content.Context
+import androidx.multidex.MultiDex
+
+
+
 
 class App : Application() {
     companion object {
@@ -12,6 +16,11 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+    }
+
+    override fun attachBaseContext(context: Context?) {
+        super.attachBaseContext(context)
+        MultiDex.install(this)
     }
 
     fun context(): Context = applicationContext
